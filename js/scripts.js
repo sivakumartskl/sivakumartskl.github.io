6216eda505b764c7a2362aa57b763db86aec72f7
sivakumartskl <sivakumartskl5a2@gmail.com>
2020-09-21 11:36:40 +0530

    A brand new portofolio website

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,21 @@
-/*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
+(function() {
+    const list = document.querySelectorAll('.container > nav > ul > li');
+    list.forEach( ( li ) => {
+        li.addEventListener('click', function() {
+            removeHighlightClass();
+            li.classList.add('highlight');
+        });
+    } );
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+    function removeHighlightClass() {
+        list.forEach( ( li ) => {
+            li.classList.remove('highlight');
+        } );
+    }
+})();
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
+function moveToSection(section) {
+    document.getElementById(section).scrollIntoView(
+        {behavior: "smooth"}
+    );
+}
